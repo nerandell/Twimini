ee5205b74cfd467637c7c6c5c4516023ba1f6d92
Nerandell <ankitchandawala@gmail.com>
2013-07-11 20:28:01 +0530

    Adding Tweet class

---
+++ TwiMini/src/main/java/com/springapp/mvc/model/Tweet.java
@@ -0,0 +1,50 @@
+package com.springapp.mvc.model;
+
+import java.security.Timestamp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: root
+ * Date: 7/11/13
+ * Time: 8:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tweet {
+
+    public String id;
+    public String username;
+    public String tweet;
+    public Timestamp timestamp;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTweet() {
+        return tweet;
+    }
+
+    public void setTweet(String tweet) {
+        this.tweet = tweet;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
