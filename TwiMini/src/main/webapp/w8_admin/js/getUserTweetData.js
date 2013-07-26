7d9933873bd535aa96f4d878a82e2257c87ce4e9
Nerandell <ankitchandawala@gmail.com>
2013-07-26 18:50:35 +0530

    Refaction in ajax for fetching tweets

--- TwiMini/src/main/webapp/w8_admin/js/getUserTweetData.js
+++ TwiMini/src/main/webapp/w8_admin/js/getUserTweetData.js
@@ -11,22 +11,24 @@ function getUserTweetData(offset, username) {
             var hour = d.getHours();
             var minutes = d.getMinutes();
             var time = curr_date + "/" + curr_month + "/" + curr_year + " " + hour + ":" + minutes;
-            var data = '<div class="itemdiv dialogdiv">';
-            data += ('<div class="user">');
-            data += ('<img alt="Alexa\'s Avatar" src="/MiniTwitter/API/users/profile_image?username='+tweet.username+'"/></div>');
-            data += ('<div class="body"><div class="time"><i class="icon-time"></i>');
-            data += ('<span class="green">');
-            data += ('<span title="'+tweet.timestamp+'">&nbsp;'+time+'</span');
-            data += ('</span>');
-            data += ('</div>');
-            data += ('<div class="name">');
-            data += ('<a href="/MiniTwitter/Website/'+tweet.username+'">'+tweet.username+'</a>');
-            data += ('</div>');
-            data += ('<div class="text">'+tweet.tweet+'</div>');
-            data += ('<div class="tools">');
-            data += ('<a href="#" class="btn btn-minier btn-info"><i class="icon-only icon-share-alt"></i></a>');
-            data += ("</div></div></div>");
-            $(data).appendTo(".tweets")
+            var data = [];
+            data.push('<div class="itemdiv dialogdiv">');
+            data.push('<div class="user">');
+            data.push('<img alt="Alexa\'s Avatar" src="/MiniTwitter/API/users/profile_image?username='+tweet.username+'"/></div>');
+            data.push('<div class="body"><div class="time"><i class="icon-time"></i>');
+            data.push('<span class="green">');
+            data.push('<span title="'+tweet.timestamp+'">&nbsp;'+time+'</span');
+            data.push('</span>');
+            data.push('</div>');
+            data.push('<div class="name">');
+            data.push('<a href="/MiniTwitter/Website/'+tweet.username+'">'+tweet.username+'</a>');
+            data.push('</div>');
+            data.push('<div class="text">'+tweet.tweet+'</div>');
+            data.push('<div class="tools">');
+            data.push('<a href="#" class="btn btn-minier btn-info"><i class="icon-only icon-share-alt"></i></a>');
+            data.push("</div></div></div>");
+            var content = data.join("")
+            $(content).appendTo(".tweets")
         });
     });
 }
