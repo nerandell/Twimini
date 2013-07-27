6387a0926b1a9ef8f0e59b17f9dbb0b35af4cb10
Nerandell <ankitchandawala@gmail.com>
2013-07-27 12:33:01 +0530

    Improving regex function to detect hashtags

--- TwiMini/target/TwiMini/w8_admin/js/processTweets.js
+++ TwiMini/target/TwiMini/w8_admin/js/processTweets.js
@@ -1,14 +1,35 @@
 function urlify(text) {
     console.log('Urlifying '+text);
-    var urlRegex = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-    return text.replace(urlRegex, function(url) {
-        var youtubeUrl = url.match(/watch\?v=([a-zA-Z0-9\-_]+)/);
-        var vimeoUrl = url.match(/^http:\/\/(www\.)?vimeo\.com\/(clip\:)?(\d+).*$/);
-        if( youtubeUrl ){
-            return '<div></div><iframe src="http://www.youtube.com/embed/'+youtubeUrl[1]+'?rel=0" height="240" width="320" allowfullscreen="" frameborder="0"></iframe></div>'
+    var re = [
+        "\\b((?:https?|ftp)://[^\\s\"'<>]+)\\b",
+        "\\b(www\\.[^\\s\"'<>]+)\\b",
+        "\\b(\\w[\\w.+-]*@[\\w.-]+\\.[a-z]{2,6})\\b",
+        "#([a-z0-9]+)"];
+    re = new RegExp(re.join('|'), "gi");
+    return text.replace(re, function(match, url, www, mail, twitler){
+        if(url) {
+            var youtubeUrl = url.match(/watch\?v=([a-zA-Z0-9\-_]+)/);
+            var vimeoUrl = url.match(/^http:\/\/(www\.)?vimeo\.com\/(clip\:)?(\d+).*$/);
+            if( youtubeUrl ){
+                return '<div></div><iframe src="http://www.youtube.com/embed/'+youtubeUrl[1]+'?rel=0" height="240" width="320" allowfullscreen="" frameborder="0"></iframe></div>'
+            }
+            else if(url.substring(0,3)=="www")
+                return '<a href="http://' + url + '">' + url + '</a>';
+            else return '<a href="' + url + '">' + url + '</a>';
         }
-        else if(url.substring(0,3)=="www")
-            return '<a href="http://' + url + '">' + url + '</a>';
-        else return '<a href="' + url + '">' + url + '</a>';
-    })
+        if(www) {
+            var youtubeUrl = www.match(/watch\?v=([a-zA-Z0-9\-_]+)/);
+            var vimeoUrl = www.match(/^http:\/\/(www\.)?vimeo\.com\/(clip\:)?(\d+).*$/);
+            if( youtubeUrl ){
+                return '<div></div><iframe src="http://www.youtube.com/embed/'+youtubeUrl[1]+'?rel=0" height="240" width="320" allowfullscreen="" frameborder="0"></iframe></div>'
+            }
+            else if(www.substring(0,3)=="www")
+                return '<a href="http://' + www + '">' + www + '</a>';
+            else return '<a href="' + www + '">' + www + '</a>';
+        }
+        if(mail)
+            return "<a href=\"mailto:" + mail + "\">" + mail + "</a>";
+        if(twitler)
+            return "<a href=\"foo?bar=" + twitler + "\">#" + twitler + "</a>";
+    });
 }
